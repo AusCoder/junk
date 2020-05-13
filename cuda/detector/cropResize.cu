9826279d457a6ce77ef37e16c320729eba20f71d
Sebastian Mueller <sebmuellermath@gmail.com>
2020-05-13 17:31:34 +1000

    feat(cuda/detector): continue cropResize

--- cuda/detector/cropResize.cu
+++ cuda/detector/cropResize.cu
@@ -5,25 +5,81 @@ Kernel to crop and resize boxes from an image
 // blockSize will be {1024, 1, 1}
 // gridSize...
 __global__ void cropResizeKernel(
-  const float *image, int imageWidth, int imageHeight, int depth
-  const float *boxes, int boxesSize,
-  int cropWidth, int cropHeight,
-  float *croppedBoxes,
-  int croppedBoxesSize // == boxesSize * cropWidth * cropHeight * depth
+    const float *image, int imageWidth, int imageHeight, int depth,
+    const float *boxes, int boxesSize, int cropWidth, int cropHeight,
+    float *croppedBoxes,
+    int croppedBoxesSize // == boxesSize * cropWidth * cropHeight * depth
 ) {
   // Assume that the depth is 3 for now
 
+  const float extrapolationValue = 0.0f;
+
   // Each thread will loop and write to certain Idx in croppedBoxes
-  for (
-    int outIdx = threadIdx.x + blockIdx.x * blockDim.x;
-    outIdx < croppedBoxesSize;
-    outIdx += blockDim.x * gridDim.x
-  ) {
+  for (int outIdx = threadIdx.x + blockIdx.x * blockDim.x;
+       outIdx < croppedBoxesSize; outIdx += blockDim.x * gridDim.x) {
     int idx = outIdx;
     const int x = idx % cropWidth;
     idx /= cropWidth;
     const int y = idx % cropHeight;
     idx /= cropHeight;
-    const int depthIdx
+    const int depthIdx = idx % depth;
+    const int boxIdx = idx / depth;
+
+    const float y1 = boxes[boxIdx * 4];
+    const float x1 = boxes[boxIdx * 4 + 1];
+    const float y2 = boxes[boxIdx * 4 + 2];
+    const float x2 = boxes[boxIdx * 4 + 3];
+
+    const int bIn = boxIdx / boxesSize;
+    if (bIn < 0 || bIn >= batch) {
+      continue;
+    }
+
+    const float heightScale =
+        (cropHeight > 1) ? (y2 - y1) * (imageHeight - 1) / (cropHeight - 1) : 0;
+    const float widthScale =
+        (cropWidth > 1) ? (x2 - x1) * (imageWidth - 1) / (cropWidth - 1) : 0;
+
+    const float inY = (cropHeight > 1)
+                          ? y1 * (imageHeight - 1) + y * heightScale
+                          : 0.5 * (y1 + y2) * (imageHeight - 1);
+    if (inY < 0 || inY > imageHeight - 1) {
+      croppedBoxes[outIdx] = extrapolationValue;
+      continue;
+    }
+
+    const float inX = (cropWidth > 1) ? x1 * (imageWidth - 1) + x * widthScale
+                                      : 0.5 * (x1 + x2) * (imageWidth - 1);
+    if (inX < 0 || inX > imageWidth - 1) {
+      croppedBoxes[outIdx] = extrapolationValue;
+      continue;
+    }
+
+    const int topYIndex = floorf(inY);
+    const int bottomYIndex = ceilf(inY);
+    const float yLerp = inY - topYIndex;
+    const int leftXIndex = floorf(inX);
+    const int rightXIndex = ceilf(inX);
+    const float xLerp = inX - leftXIndex;
+
+    const float topLeft(static_cast<float>(
+        image[((bIn * depth + depthIdx) * imageHeight + topYIndex) *
+                  imageWidth +
+              leftXIndex]));
+    const float topRight(static_cast<float>(
+        image[((bIn * depth + depthIdx) * imageHeight + topYIndex) *
+                  imageWidth +
+              rightXIndex]));
+    const float bottomLeft(static_cast<float>(
+        image[((bIn * depth + depthIdx) * imageHeight + bottomYIndex) *
+                  imageWidth +
+              leftXIndex]));
+    const float bottomRight(static_cast<float>(
+        image[((bIn * depth + depthIdx) * imageHeight + bottomYIndex) *
+                  imageWidth +
+              rightXIndex]));
+    const float top = topLeft + (topRight - topLeft) * xLerp;
+    const float bottom = bottomLeft + (bottomRight - bottomLeft) * xLerp;
+    croppedBoxes[out_idx] = top + (bottom - top) * yLerp;
   }
 }
