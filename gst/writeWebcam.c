08c4a69af24d990d7b7feafb0f3e0eb3c17f27b7
AusCoder <sebmuellermath@gmail.com>
2020-07-15 08:44:31 +1000

    feat(gst): clean pu

--- gst/writeWebcam.c
+++ gst/writeWebcam.c
@@ -1,31 +1,29 @@
-#include <iostream>
-
-#include <gst/gst.h>
 #include <glib.h>
+#include <gst/gst.h>
 
 static gboolean bus_call(GstBus *bus, GstMessage *msg, gpointer data) {
   GMainLoop *loop = (GMainLoop *)data;
 
   switch (GST_MESSAGE_TYPE(msg)) {
-    case GST_MESSAGE_EOS:
-      g_print ("End of stream\n");
-      g_main_loop_quit(loop);
-      break;
-    case GST_MESSAGE_ERROR: {
-      gchar *debug;
-      GError *error;
+  case GST_MESSAGE_EOS:
+    g_print("End of stream\n");
+    g_main_loop_quit(loop);
+    break;
+  case GST_MESSAGE_ERROR: {
+    gchar *debug;
+    GError *error;
 
-      gst_message_parse_error(msg, &error, &debug);
-      g_free(debug);
+    gst_message_parse_error(msg, &error, &debug);
+    g_free(debug);
 
-      g_printerr("Error: %s\n", error->message);
-      g_error_free(error);
+    g_printerr("Error: %s\n", error->message);
+    g_error_free(error);
 
-      g_main_loop_quit(loop);
-      break;
-    }
-    default:
-      break;
+    g_main_loop_quit(loop);
+    break;
+  }
+  default:
+    break;
   }
 
   return TRUE;
@@ -46,7 +44,10 @@ int main(int argc, char *argv[]) {
 
   GstElement *pipeline, *source, *videoconvert, *encoder, *mux, *sink;
   GstBus *bus;
-  guint bus_watch_id;
+  guint bus_watch_id, major, minor, micro, nano;
+
+  gst_version(&major, &minor, &micro, &nano);
+  g_print("Using Gstreamer version: %d.%d.%d.%d\n", major, minor, micro, nano);
 
   gst_init(&argc, &argv);
   loop = g_main_loop_new(NULL, FALSE);
@@ -63,7 +64,7 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
-  g_object_set(G_OBJECT(source), "num-buffers", 5, NULL);
+  g_object_set(G_OBJECT(source), "num-buffers", 50, NULL);
   g_object_set(G_OBJECT(source), "device", "/dev/video0", NULL);
   g_object_set(G_OBJECT(sink), "location", "test.mp4", NULL);
   g_object_set(G_OBJECT(sink), "async", FALSE, NULL);
@@ -72,8 +73,8 @@ int main(int argc, char *argv[]) {
   bus_watch_id = gst_bus_add_watch(bus, bus_call, loop);
   gst_object_unref(bus);
 
-  gst_bin_add_many(GST_BIN(pipeline),
-    source, videoconvert, encoder, mux, sink, NULL);
+  gst_bin_add_many(GST_BIN(pipeline), source, videoconvert, encoder, mux, sink,
+                   NULL);
 
   gst_element_link_many(source, videoconvert, encoder, mux, sink, NULL);
 
