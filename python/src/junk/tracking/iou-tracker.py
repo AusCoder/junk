2ab7faf2489e590a2622abbb4fe0d0d20da0c56a
Sebastian Mueller <sebastian.mueller@nirovision.com>
2020-09-11 09:45:11 +1000

    feat(tracking): improve iou tracker

--- python/src/junk/tracking/iou-tracker.py
+++ python/src/junk/tracking/iou-tracker.py
@@ -1,3 +1,4 @@
+import sys
 import time
 from collections import namedtuple
 from typing import Any, Iterable, List, Optional
@@ -5,20 +6,19 @@ from typing import Any, Iterable, List, Optional
 import cv2
 import numpy as np
 import matplotlib.cm as cm
+from scipy import optimize
+
 
 KEY_ESC = 27
+IMSHOW_SLEEP_TIME = 500
 
 COLORS = (cm.jet(np.linspace(0, 1.0, 11))[:, :3] * 256).astype(int)
-
 BOX_THICKNESS = 2
 MEASUREMENT_COLOR = (0, 255, 0)
 TRACK_COLORS = COLORS
 
 IOU_THRESHOLD = 0.1
 TRACKER_MAX_AGE = 3
-
-
-PBox = np.ndarray
 
 
 class IoUTracker:
@@ -44,13 +44,22 @@ class IoUTracker:
         self.predictions_since_update = 0
 
 
-def run_tracking():
+def run_tracking(measurement_type: str) -> None:
     image_arr = np.ones((720, 1280, 3), dtype=np.uint8) * 100
 
     # Simulated measurements
-    all_measurements = get_sliding_boxes_measurements(image_arr)
-    # all_measurements = get_constant_velocity_circle_measurements(image_arr)
-    # all_measurements = get_accelerating_circle_measurements(image_arr)
+    if measurement_type == "slide":
+        all_measurements = get_sliding_boxes_measurements(image_arr)
+    elif measurement_type == "circle":
+        all_measurements = get_circle_measurements(image_arr, acceleration=1.0)
+    elif measurement_type == "circle-accel":
+        all_measurements = get_circle_measurements(image_arr, acceleration=1.1)
+    elif measurement_type == "collide":
+        all_measurements = get_collide_measurements(image_arr)
+    else:
+        raise ValueError(
+            f"Invalid measurement type: {measurement_type}. Expected one of: slide, circle, circle-accel, collide"
+        )
 
     # Current set of trackers
     trackers = []
@@ -89,100 +98,120 @@ def get_sliding_boxes_measurements(image_arr: np.ndarray) -> List[np.ndarray]:
     return all_measurements
 
 
-def get_constant_velocity_circle_measurements(
-    image_arr: np.ndarray,
+def get_circle_measurements(
+    image_arr: np.ndarray, acceleration: float = 1.0
 ) -> List[np.ndarray]:
     measurements_length = 30
-    angle = np.pi / 24
-    box_hw = 100
+    angular_velocity = np.pi / 24
+    box_size = 100
     img_h, img_w, _ = image_arr.shape
     center_x, center_y = img_w // 2, img_h // 2
-    radius = min(img_h, img_w) // 2 - box_hw
+    radius = min(img_h, img_w) // 2 - box_size
 
     all_measurements = []
     for i in range(measurements_length):
-        x1, y1 = (
-            center_x + radius * np.sin(i * angle),
-            center_y + radius * np.cos(i * angle),
-        )
-        x2, y2 = (
-            center_x + radius * np.sin(np.pi + i * angle),
-            center_y + radius * np.cos(np.pi + i * angle),
-        )
+        top_left_points = [
+            (
+                center_x + radius * np.sin(np.pi * 2 * j / 3 + i * angular_velocity),
+                center_y + radius * np.cos(np.pi * 2 * j / 3 + i * angular_velocity),
+            )
+            for j in range(3)
+        ]
         measurements = np.array(
-            [[x1, y1, x1 + box_hw, y1 + box_hw], [x2, y2, x2 + box_hw, y2 + box_hw]]
+            [[x, y, x + box_size, y + box_size] for x, y in top_left_points]
         ).astype(int)
         all_measurements.append(measurements)
+        angular_velocity *= acceleration
     return all_measurements
 
 
-def get_accelerating_circle_measurements(image_arr: np.ndarray,) -> List[np.ndarray]:
-    measurements_length = 30
-    angle = np.pi / 24
-    accel = 1.05
-    box_hw = 100
+def get_collide_measurements(image_arr: np.ndarray) -> List[np.ndarray]:
     img_h, img_w, _ = image_arr.shape
-    center_x, center_y = img_w // 2, img_h // 2
-    radius = min(img_h, img_w) // 2 - box_hw
+    measurements_length = 30
+    box_size = 100
+    velocity = img_w // measurements_length
 
     all_measurements = []
     for i in range(measurements_length):
-        x1, y1 = (
-            center_x + radius * np.sin(i * angle),
-            center_y + radius * np.cos(i * angle),
-        )
-        x2, y2 = (
-            center_x + radius * np.sin(np.pi + i * angle),
-            center_y + radius * np.cos(np.pi + i * angle),
-        )
+        left_position = i * velocity
+        top_position = img_h // 2
         measurements = np.array(
-            [[x1, y1, x1 + box_hw, y1 + box_hw], [x2, y2, x2 + box_hw, y2 + box_hw]]
-        ).astype(int)
+            [
+                [
+                    left_position,
+                    top_position,
+                    left_position + box_size,
+                    top_position + box_size,
+                ],
+                [
+                    img_w - box_size - (left_position + box_size),
+                    top_position,
+                    img_w - box_size - left_position,
+                    top_position + box_size,
+                ],
+            ]
+        )
         all_measurements.append(measurements)
-        angle *= accel
     return all_measurements
 
 
 def predict_match_create_update_and_kill(
     measurements: np.ndarray, trackers: List[IoUTracker]
 ) -> List[IoUTracker]:
-    # Here we want to match the measurements to the trackers
-    # Logic:
-    #   Predict tracker forward
-    #   for each measurement, find the tracker that we match the best
-    #       if found, update with that measurement
-    #       otherwise create new tracker
-    #   evict old trackers
-
+    # Tracker propogation
     for tracker in trackers:
         tracker.predict()
 
+    # Cost calculation and assignment
     tracker_arr = np.empty((0, 4), dtype=float)
     if trackers:
         tracker_arr = np.stack([tracker.box for tracker in trackers])
+    cost_matrix = calculate_cost_matrix(measurements, tracker_arr)
+    measurement_assignments, tracker_assignments = optimize.linear_sum_assignment(
+        cost_matrix
+    )
+    # We just want assignments where the IoU is > 0
+    valid_assignments = [
+        (measurement_idx, tracker_idx)
+        for measurement_idx, tracker_idx in zip(
+            measurement_assignments, tracker_assignments
+        )
+        if cost_matrix[measurement_idx, tracker_idx] < 0
+    ]
 
-    tracker_arr_idxs = np.arange(len(tracker_arr))
-    new_trackers = []
-    for measurement in measurements:
-        iou = calculate_iou(measurement, tracker_arr[tracker_arr_idxs])
+    # Update tracker based on assignments
+    for measurement_idx, tracker_idx in valid_assignments:
+        trackers[tracker_idx].update(measurements[measurement_idx])
 
-        if iou.size == 0 or np.allclose(iou, np.zeros_like(iou)):
-            new_trackers.append(IoUTracker(measurement))
-        else:
-            max_iou_idx = tracker_arr_idxs[iou.argmax()]
-            trackers[max_iou_idx].update(measurement)
-            tracker_arr_idxs = tracker_arr_idxs[tracker_arr_idxs != max_iou_idx]
-
-    return [
+    # Create new trackers
+    valid_measurement_assignments = set(m for m, _ in valid_assignments)
+    new_trackers = [
+        IoUTracker(measurement)
+        for i, measurement in enumerate(measurements)
+        if i not in valid_measurement_assignments
+    ]
+    # Evict old trackers
+    live_trackers = [
         tracker
         for tracker in trackers + new_trackers
         if tracker.predictions_since_update < TRACKER_MAX_AGE
     ]
+    return live_trackers
+
+
+def calculate_cost_matrix(measurements: np.ndarray, trackers: np.ndarray) -> np.ndarray:
+    assert measurements.ndim == 2
+    assert trackers.ndim == 2
+
+    cost_matrix = np.empty((len(measurements), len(trackers)), dtype=float)
+    for i, measurement in enumerate(measurements):
+        cost_matrix[i, :] = -calculate_iou(measurement, trackers)
+    return cost_matrix
 
 
 def calculate_iou(b1: np.ndarray, b2: np.ndarray) -> np.ndarray:
     if b2.size == 0:
-        return np.empty((0, 4), dtype=float)
+        return np.empty((0), dtype=float)
     if b2.ndim == 1:
         b2 = b2.reshape((1, -1))
     assert b1.shape == (4,)
@@ -201,7 +230,7 @@ def calculate_iou(b1: np.ndarray, b2: np.ndarray) -> np.ndarray:
 
 
 def draw_bounding_boxes(
-    image_arr: np.ndarray, bounding_boxes: Iterable[PBox], *, color: Any
+    image_arr: np.ndarray, bounding_boxes: Iterable[np.ndarray], *, color: Any
 ) -> np.ndarray:
     new_image_arr = np.array(image_arr, copy=True)
     for box in bounding_boxes:
@@ -218,18 +247,21 @@ def draw_bounding_boxes(
 
 def draw_and_show_bounding_boxes(
     image_arr: np.ndarray,
-    bounding_boxes: Iterable[PBox],
+    bounding_boxes: Iterable[np.ndarray],
     *,
     color: Any,
     should_wait: bool = False,
-    sleep_time_ms: int = 500,
+    sleep_time_ms: int = IMSHOW_SLEEP_TIME,
 ) -> None:
     new_image_arr = draw_bounding_boxes(image_arr, bounding_boxes, color=color)
     show_image(new_image_arr, should_wait=should_wait, sleep_time_ms=sleep_time_ms)
 
 
 def show_image(
-    image_arr: np.ndarray, *, should_wait: bool = False, sleep_time_ms: int = 500
+    image_arr: np.ndarray,
+    *,
+    should_wait: bool = False,
+    sleep_time_ms: int = IMSHOW_SLEEP_TIME,
 ) -> None:
     cv2.imshow("window", image_arr)
 
@@ -243,5 +275,6 @@ def show_image(
 
 
 if __name__ == "__main__":
-    run_tracking()
+    measurement_type = sys.argv[1] if len(sys.argv) > 1 else "circle"
+    run_tracking(measurement_type)
     cv2.destroyAllWindows()
