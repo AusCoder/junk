09f5fd531e5767be3cac4dc9ff62c48ec496563e
Sebastian Mueller <sebastian.mueller@nirovision.com>
2020-10-23 09:15:55 +1100

    feat(xor-linked-list): get working

--- cpp/xor-doublely-linked-list.c
+++ cpp/xor-doublely-linked-list.c
@@ -1,9 +1,17 @@
+/* A doubly linked list where each node stores links
+  as add(prev) ^ addr(next). Using properties of xor,
+  we can traverse the list as normal.
+  It uses a bit less space than holding forward and backward
+  pointers on each node.
+ */
 #include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define CURSOR_CURRENT(cursor) ((Node *)((cursor).current))
+#define NODE_PTR_AS_ADDR(ptr) ((uintptr_t)(ptr))
+#define ADDR_AS_NODE_PTR(addr) ((Node *)(addr))
+#define CURSOR_CURRENT(cursor) (ADDR_AS_NODE_PTR((cursor).current))
 #define CURSOR_CURRENT_PREVNEXT(cursor) (CURSOR_CURRENT(cursor))->prevnext
 #define CURSOR_CURRENT_DATA(cursor) (CURSOR_CURRENT(cursor))->data
 
@@ -41,99 +49,57 @@ Cursor create_singleton_list(int value) {
 }
 
 void append(Cursor head, void *d) {
-  // Cursor c = head;
   while (1) {
-    // uintptr_t prevnext = ((Node *)c.current)->prevnext;
     uintptr_t next = head.previous ^ CURSOR_CURRENT_PREVNEXT(head);
     if (next == 0) {
       break;
     }
     head.previous = head.current;
     head.current = next;
-    // head = {head.current, next};
-    // c.previous = c.current;
-    // c.current = next;
   }
   Node *new = (Node *)malloc(sizeof(Node));
   assert(new != NULL);
-  uintptr_t newIntPrt = (uintptr_t)(new);
+  CURSOR_CURRENT_PREVNEXT(head) = head.previous ^ NODE_PTR_AS_ADDR(new);
   new->prevnext = head.current ^ 0;
   new->data = d;
-  ((Node *)head.current)->prevnext = newIntPrt ^ 0;
-  // printf("End\n");
-
-  // while (head->prevnext != 0) {
-  //   head = (Node *)head->prevnext;
-  // }
-  // Node *next = (Node *)malloc(sizeof(Node));
-  // assert(next != NULL);
-  // next->prevnext = 0;
-  // next->data = d;
-  // head->prevnext = (uintptr_t)next;
 }
 
-// void append(Node *head, void *d) {
-//   while (head->prevnext != 0) {
-//     head = (Node *)head->prevnext;
-//   }
-//   Node *next = (Node *)malloc(sizeof(Node));
-//   assert(next != NULL);
-//   next->prevnext = 0;
-//   next->data = d;
-//   head->prevnext = (uintptr_t)next;
-// }
-
-void free_list(Node *head) {
-  uintptr_t currentIntPtr = (uintptr_t)head;
-  while (currentIntPtr != 0) {
-    Node *currentPtr = (Node *)currentIntPtr;
-    free(currentPtr->data);
-    currentIntPtr = currentPtr->prevnext;
-    free(currentPtr);
+void free_list(Cursor head) {
+  while (head.current != 0) {
+    if (CURSOR_CURRENT_DATA(head)) {
+      free(CURSOR_CURRENT_DATA(head));
+    }
+    Node *tmp = CURSOR_CURRENT(head);
+    uintptr_t next = CURSOR_CURRENT_PREVNEXT(head) ^ head.previous;
+    head.previous = head.current;
+    head.current = next;
+    // head.current->prevnext is broken as this point, but
+    // we are freeing the list, so it's not a big deal. Also
+    // head.previous is pointing to freed memory...
+    // We'd want:
+    // next->prevnext = 0 ^ (next->prevnext ^ head.current)
+    // head.previous = 0
+    // Or something like this...
+    free(tmp);
   }
 }
 
 void print_list(Cursor head) {
-  Cursor cursor = head;
-  while (1) {
-    uintptr_t prevnext = CURSOR_CURRENT_PREVNEXT(cursor);
-    uintptr_t next = cursor.previous ^ prevnext;
-    if (next == 0) {
-      break;
-    }
-    int value = ((IntData *)CURSOR_CURRENT_DATA(cursor))->value;
+  while (head.current != 0) {
+    int value = ((IntData *)CURSOR_CURRENT_DATA(head))->value;
     printf("%d, ", value);
-    cursor.previous = cursor.current;
-    cursor.current = next;
+    uintptr_t next = CURSOR_CURRENT_PREVNEXT(head) ^ head.previous;
+    head.previous = head.current;
+    head.current = next;
   }
-
-  // uintptr_t currentIntPtr = (uintptr_t)head;
-  // while (currentIntPtr != 0) {
-  //   Node *currentPtr = (Node *)currentIntPtr;
-  //   int value = ((IntData *)(currentPtr->data))->value;
-  //   printf("%d, ", value);
-  //   currentIntPtr = currentPtr->prevnext;
-  // }
-  // printf("\n");
+  printf("\n");
 }
-
-// void print_list(Node *head) {
-//   uintptr_t currentIntPtr = (uintptr_t)head;
-//   while (currentIntPtr != 0) {
-//     Node *currentPtr = (Node *)currentIntPtr;
-//     int value = ((IntData *)(currentPtr->data))->value;
-//     printf("%d, ", value);
-//     currentIntPtr = currentPtr->prevnext;
-//   }
-//   printf("\n");
-// }
 
 int main(int argc, char *argv[]) {
   Cursor head = create_singleton_list(4);
   append(head, create_int_data(5));
+  append(head, create_int_data(6));
   print_list(head);
-  // append(head, create_int_data(6));
-  // print_list(head);
-  // free_list(head);
+  free_list(head);
   return 0;
 }
