5da0decc25157a8b99a169432e19fe4cc9a49b1c
Sebastian Mueller <sebastian.mueller@nirovision.com>
2020-11-12 07:44:40 +1100

    feat(hackerrank): continue magic square

--- algos/hackerrank/magic-square.cc
+++ algos/hackerrank/magic-square.cc
@@ -1,10 +1,54 @@
 #include <algorithm>
+#include <array>
 #include <iostream>
 #include <iterator>
 #include <sstream>
 #include <vector>
 
-void formMagicSquare(std::vector<std::vector<int>> matrix) {}
+// [
+//   [9, 1, 5], // sum 15
+//   [8, 4, 3], // sum 15
+//   [], // sum 15
+// ]
+
+void printMatrix(const std::vector<std::vector<int>> &matrix) {
+  for (auto &row : matrix) {
+    for (auto &x : row) {
+      std::cout << x << " ";
+    }
+    std::cout << "\n";
+  }
+}
+
+bool isSolved(const std::vector<std::vector<int>> &matrix) {
+  std::array<bool, 9> hits;
+  std::fill(hits.begin(), hits.end(), false);
+
+  for (int y = 0; y < 3; y++) {
+    for (int x = 0; x < 3; x++) {
+      hits.at(matrix[y][x] - 1) = true;
+    }
+  }
+  bool hitAllNums = true;
+  for (auto &x : hits) {
+    hitAllNums &= x;
+  }
+  return hitAllNums;
+}
+
+void formMagicSquare(std::vector<std::vector<int>> matrix) {
+  for (int y = 0; y < 3; y++) {
+    for (int x = 0; x < 3; x++) {
+      // if isSolved(matrix): return 0;
+      // else:
+      //     want to try and set a number
+      // for n in range(1, 10):
+      //   cost = abs(matrix[y][x] - n)
+      //   matrix[y][x] = n
+      //   return cost
+    }
+  }
+}
 
 int main() {
   std::string line;
@@ -18,8 +62,9 @@ int main() {
     }
     std::istringstream iss(line);
 
-    std::vector<std::string> elems((std::istream_iterator<std::string>(iss)),
-                                   std::istream_iterator<std::string>());
+    auto startIter = std::istream_iterator<std::string>(iss);
+    auto endIter = std::istream_iterator<std::string>();
+    std::vector<std::string> elems(startIter, endIter);
     // XXX: Without the parens, it is a most vexing parse
     // std::vector<std::string> elems(std::istream_iterator<std::string>(iss),
     //                                std::istream_iterator<std::string>());
@@ -29,8 +74,9 @@ int main() {
 
     matrix.push_back(row);
   }
-
-  // for (auto &s : row) {
+  printMatrix(matrix);
+  std::cout << isSolved(matrix) << "\n";
+  // for (auto &s : matrix[0]) {
   //   std::cout << s << " - ";
   // }
   // std::cout << "\n";
