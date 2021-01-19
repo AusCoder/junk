574d474dbc95637474abe81a0182fdb6d9e19ae7
Sebastian Mueller <sebastian.mueller@nirovision.com>
2021-01-19 18:02:58 +1100

    feat(python): add cython module

--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,30 @@
+"""
+Useful build commands:
+    # build extensions
+    python setup.py build_ext
+
+    # build extensions and copy so we can do editable install
+    python setup.py build_ext --inplace
+
+    # clean
+    python setup.py clean
+"""
+
 from setuptools import setup, find_packages, Extension
 
-spam_extension = Extension("spam", sources=["src/junk/spammodule.c"])
+import numpy as np
+from Cython.Build import cythonize
+
+spam_extension = Extension("junk.spam", sources=["src/junk/spammodule.c"])
+cython_modules = cythonize(
+    [
+        Extension(
+            "junk.mandel",
+            sources=["src/junk/mandel.pyx"],
+            include_dirs=[np.get_include()],
+        )
+    ]
+)
 
 setup(
     name="junk",
@@ -9,5 +33,5 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=["black", "pytest", "pytest-cov", "pylint"],
-    ext_modules=[spam_extension],
+    ext_modules=[spam_extension, *cython_modules],
 )
