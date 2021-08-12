63ad880d20f7a88448ba16a2ffaaca97cc32a2d8
AusCoder <sebmuellermath@gmail.com>
2021-08-13 09:07:27 +1000

    feat(c): add simple glib-value example

---
+++ c/glib-value.c
@@ -0,0 +1,9 @@
+#include <glib.h>
+#include <glib-object.h>
+
+int main(int argc, char **argv) {
+  GValue value;
+  g_value_init(&value, G_TYPE_INT);
+  g_value_set_int(&value, 100);
+  g_print("Value is %d\n", g_value_get_int(&value));
+}
